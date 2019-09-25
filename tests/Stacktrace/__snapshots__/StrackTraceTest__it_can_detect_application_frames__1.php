9a68aa4cea7aadc419d339f2a44d314cfe92fd82
Marcel Pociot <m.pociot@gmail.com>
2019-09-25 11:22:29 +0200

    wip

---
+++ tests/Stacktrace/__snapshots__/StrackTraceTest__it_can_detect_application_frames__1.php
@@ -0,0 +1,7 @@
+<?php return Facade\FlareClient\Stacktrace\Frame::__set_state(array(
+   'file' => '/Users/marcel/Code/flare-testapp/app/User.php',
+   'lineNumber' => 42,
+   'method' => 'doSomething',
+   'class' => 'App\\User',
+   'isApplicationFrame' => true,
+));
